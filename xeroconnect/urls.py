75d77384242dc95415747b1ba7d28115c9ff883e
woodhack <95056320+woodhack@users.noreply.github.com>
2022-07-16 11:28:14 +1000

    initial commit

---
+++ xeroconnect/urls.py
@@ -0,0 +1,29 @@
+"""xeroWeb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('home', index, name='home'),
+    path('login', login, name='login'),
+    path('callback', oauth_callback, name='callback'),
+    path('logout', logout, name='logout'),
+    path('tenants', tenants, name='tenants'),
+    #path('export-token', export_token, name='export_token'),
+    #path('refresh-token', refresh_token, name='refresh_token'),
+    path('accounting-get-report-profit-and-loss', accounting_get_report_profit_and_loss, name='accounting_get_report_profit_and_loss'),
+    path('accounting-get-report-balance-sheet', accounting_get_report_balance_sheet, name='accounting_get_report_balance_sheet'),
+]
---
+++ xeroconnect/views.py
@@ -0,0 +1,247 @@
+import traceback
+import datetime
+import json
+import dateutil
+from django.http import JsonResponse
+from decimal import Decimal
+
+from dateutil import tz
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.sessions.models import Session
+
+from functools import wraps
+from django.http import HttpResponseRedirect
+
+from authlib.integrations.django_client import OAuth
+
+# Create your views here.
+# configure xero-python sdk client
+from xero_python.accounting import AccountingApi, Account, Accounts, AccountType, Allocation, Allocations, BatchPayment, BatchPayments, BankTransaction, BankTransactions, BankTransfer, BankTransfers, Contact, Contacts, ContactGroup, ContactGroups, ContactPerson, CreditNote, CreditNotes, Currency, Currencies, CurrencyCode, Employee, Employees, ExpenseClaim, ExpenseClaims, HistoryRecord, HistoryRecords, Invoice, Invoices, Item, Items, LineAmountTypes, LineItem, Payment, Payments, PaymentService, PaymentServices, Phone, Purchase, Receipt, Receipts, TaxComponent, TaxRate, TaxRates, TaxType, TrackingCategory, TrackingCategories, TrackingOption, TrackingOptions, User, Users
+from xero_python.assets import AssetApi, Asset, AssetStatus, AssetStatusQueryParam, AssetType, BookDepreciationSetting
+from xero_python.project import ProjectApi, Amount, ChargeType, Projects, ProjectCreateOrUpdate, ProjectPatch, ProjectStatus, ProjectUsers, Task, TaskCreateOrUpdate, TimeEntryCreateOrUpdate
+from xero_python.payrollau import PayrollAuApi, Employees, Employee, EmployeeStatus,State, HomeAddress
+from xero_python.payrolluk import PayrollUkApi, Employees, Employee, Address, Employment
+from xero_python.payrollnz import PayrollNzApi, Employees, Employee, Address, Employment, EmployeeLeaveSetup
+from xero_python.file import FilesApi
+from xero_python.finance import FinanceApi
+from xero_python.api_client import ApiClient, serialize
+from xero_python.api_client.configuration import Configuration
+from xero_python.api_client.oauth2 import OAuth2Token
+from xero_python.exceptions import AccountingBadRequestException, PayrollUkBadRequestException
+from xero_python.identity import IdentityApi
+from xero_python.utils import getvalue
+
+from .utils import jsonify, serialize_model
+
+# Configure oauth using authlib
+oauth = OAuth()
+xero = oauth.register(
+    name="xero",
+    version="2",
+    client_id="B8088CE4C926465AAAFBC99D6CAED1BC",  # Should be changed
+    client_secret="tgl0-CgUlT6MInzgyuTRco2hYtOQis7xAaOuQuEy8FhY0JY0", # Should be changed
+    endpoint_url="https://api.xero.com/",
+    authorize_url="https://login.xero.com/identity/connect/authorize",
+    access_token_url="https://identity.xero.com/connect/token",
+    refresh_token_url="https://identity.xero.com/connect/token",
+    # scope="offline_access openid profile email",
+    scope="offline_access openid profile email accounting.transactions "
+    "accounting.reports.read accounting.journals.read accounting.settings "
+    "accounting.contacts accounting.attachments assets projects",
+    server_metadata_url="https://identity.xero.com/.well-known/openid-configuration",
+)
+
+
+# configure xero-python sdk client
+api_client = ApiClient(
+    Configuration(
+        debug=True,
+        oauth2_token=OAuth2Token(
+            client_id="B8088CE4C926465AAAFBC99D6CAED1BC", # Keep secure
+            client_secret="tgl0-CgUlT6MInzgyuTRco2hYtOQis7xAaOuQuEy8FhY0JY0"  # Keep secure
+        ),
+    ),
+    pool_threads=1,
+)
+
+# Token details stored below, cosider changing to session etc. 
+token_details = {'token': None,
+                 'modified': True}
+
+
+@api_client.oauth2_token_getter
+def obtain_xero_oauth2_token():
+    return token_details["token"]
+
+
+@api_client.oauth2_token_saver
+def store_xero_oauth2_token(token):
+    token_details["token"] = token
+    token_details['modified'] = True
+
+def xero_token_required(function):
+    @wraps(function)
+    def decorator(*args, **kwargs):
+        xero_token = obtain_xero_oauth2_token()
+        if not xero_token:
+            return redirect("login")
+
+        return function(*args, **kwargs)
+
+    return decorator
+
+@xero_token_required
+def index(request):
+    try:
+        xero_access = dict(obtain_xero_oauth2_token() or {})
+        return render(request, template_name='code.html')
+        api_client.refresh_oauth2_token()
+        return get_invoices(request)
+    except Exception as e:
+        print(e)
+        return redirect("login")
+
+def login(request):
+    if not obtain_xero_oauth2_token():
+        response = xero.authorize_redirect(request, redirect_uri="http://localhost:8000/callback")
+        return response
+    return redirect("home")
+
+
+def oauth_callback(request):
+    try:
+        response = xero.authorize_access_token(request)
+    except Exception as e:
+        print(e)
+        raise
+    # todo validate state value
+    if response is None or response.get("access_token") is None:
+        return "Access denied: response=%s" % response
+    store_xero_oauth2_token(response)
+    return redirect ("home")
+
+def logout(request):
+    store_xero_oauth2_token(None)
+    return redirect("login")
+
+def get_connection_id():
+    identity_api = IdentityApi(api_client)
+    for connection in identity_api.get_connections():
+        if connection.tenant_type == "ORGANISATION":
+            return connection.id
+
+@xero_token_required
+def tenants(request):
+    identity_api = IdentityApi(api_client)
+    accounting_api = AccountingApi(api_client)
+    asset_api = AssetApi(api_client)
+
+    available_tenants = []
+    for connection in identity_api.get_connections():
+        tenant = serialize(connection)
+        if connection.tenant_type == "ORGANISATION":
+            organisations = accounting_api.get_organisations(
+                xero_tenant_id=connection.tenant_id
+            )
+            tenant["organisations"] = serialize(organisations)
+
+        available_tenants.append(tenant)
+
+    return render(request, template_name=
+        'output.html'
+    )
+
+def get_xero_tenant_id():
+    token = obtain_xero_oauth2_token()
+    if not token:
+        return None
+
+    identity_api = IdentityApi(api_client)
+    for connection in identity_api.get_connections():
+        if connection.tenant_type == "ORGANISATION":
+            return connection.tenant_id
+    
+# Get Profit and Loss
+
+@xero_token_required
+def accounting_get_report_profit_and_loss(request):
+    api_instance = AccountingApi(api_client)
+    xero_tenant_id = get_xero_tenant_id()
+    from_date = dateutil.parser.parse("2021-10-31")
+    to_date = dateutil.parser.parse("2022-05-31")
+    periods = '1'
+    timeframe = 'MONTH'
+    tracking_category_id = ''
+    tracking_category_id_2 = ''
+    tracking_option_id = ''
+    tracking_option_id_2 = ''
+    standard_layout = 'True'
+    payments_only = 'False'
+    
+    try:
+        api_response = api_instance.get_report_profit_and_loss(xero_tenant_id, from_date, to_date, periods, timeframe, tracking_category_id, tracking_category_id_2, tracking_option_id, tracking_option_id_2, standard_layout, payments_only)
+        print(api_response)
+        return render(request, template_name="code.html")
+    except AccountingBadRequestException as e:
+        print("Exception when calling AccountingApi->getReportProfitAndLoss: %s\n" % e)
+
+# Get Balance Sheet
+def accounting_get_report_balance_sheet(request):
+    api_instance = AccountingApi(api_client)
+    xero_tenant_id = get_xero_tenant_id()
+    date = dateutil.parser.parse("2022-05-01")
+    periods = '1'
+    timeframe = 'MONTH'
+    tracking_option_id_1 = ''
+    tracking_option_id_2 = ''
+    standard_layout = 'True'
+    payments_only = 'False'
+    
+    try:
+        api_response = api_instance.get_report_balance_sheet(xero_tenant_id, date, periods, timeframe, tracking_option_id_1, tracking_option_id_2, standard_layout, payments_only)
+        print(api_response)
+        return render(request, template_name="code.html")
+    except AccountingBadRequestException as e:
+        print("Exception when calling AccountingApi->getReportBalanceSheet: %s\n" % e)
+
+"""
+# Get contacts
+def accounting_get_contacts():
+    api_instance = AccountingApi(api_client)
+    xero_tenant_id = 'YOUR_XERO_TENANT_ID'
+    if_modified_since = dateutil.parser.parse("2020-02-06T12:17:43.202-08:00")
+    where = 'ContactStatus=="ACTIVE"'
+    order = 'Name ASC'
+    ids = ["00000000-0000-0000-0000-000000000000"]
+    include_archived = 'True'
+    summary_only = 'True'
+    search_term = 'searchTerm=Joe Bloggs'
+    
+    try:
+        api_response = api_instance.get_contacts(xero_tenant_id, if_modified_since, where, order, ids, page, include_archived, summary_only, search_term)
+        print(api_response)
+    except AccountingBadRequestException as e:
+        print("Exception when calling AccountingApi->getContacts: %s\n" % e)
+"""
+
+"""
+# Get Invoices
+@xero_token_required
+def get_invoices(request):
+    xero_tenant_id = get_xero_tenant_id()
+    print("Tenent Id")
+    print(xero_tenant_id)
+    accounting_api = AccountingApi(api_client)
+    print(dir(accounting_api))
+    invoices = accounting_api.get_invoices(
+        xero_tenant_id
+    )
+    print(invoices)
+    code = serialize_model(invoices)
+    sub_title = "Total invoices found: {}".format(len(invoices.invoices))
+    context = {'title': 'xero|Invoices',
+               'code': code['Invoices'],
+               'sub_title': sub_title}
+    return render(request, template_name="code.html", context=context)
+"""
